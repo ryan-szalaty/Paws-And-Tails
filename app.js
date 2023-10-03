d5fb29f7d94ac3b609be8b54282e92e0733a4350
ryan-szalaty <ryan.szalaty@gmail.com>
2023-10-03 10:32:28 +0900

    Started homepage.

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 require('dotenv').config();
 
 const express = require("express");
-
+const bodyParser = require('body-parser');
+const path = require('path');
 const app = express();
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
-    res.send("Everything is working!");
+    res.render("index");
 });
 
 app.listen(process.env.PORT || 8000, () => {
