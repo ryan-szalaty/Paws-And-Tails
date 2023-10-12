0ecddf0abb5837b93cbb7dceb3ca44b2887322b4
ryan-szalaty <ryan.szalaty@gmail.com>
2023-10-12 12:08:58 +0900

    Fixed bugs.

--- app.js
+++ app.js
@@ -3,9 +3,20 @@ require("dotenv").config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
+const session = require("express-session");
+const flash = require('express-flash');
+const secret = process.env.SECRET_KEY;
 const mongoose = require("mongoose");
-const { create } = require("domain");
+const axios = require('axios');
 const app = express();
+
+app.use(session({
+    secret,
+    resave: false,
+    saveUninitialized: true,
+}));
+
+app.use(flash());
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
@@ -14,66 +25,68 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 async function connect() {
-  try {
-    await mongoose.connect(
-      `mongodb+srv://rszalaty:${process.env.MONGODB_PASS}@cluster0.kuzogfn.mongodb.net/`,
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    );
-    console.log(`Connected to database.`);
-  } catch (error) {
-    console.log(`Error Description: ${error}`);
-  }
+    try {
+        await mongoose.connect(
+            `${process.env.MONGODB_URL}`,
+            {
+                useNewUrlParser: true,
+                useUnifiedTopology: true,
+            }
+        );
+        console.log(`Connected to database.`);
+    } catch (error) {
+        console.log(`Error Description: ${error}`);
+    }
 }
 
 const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: true,
-    unique: true,
-  },
-  preference: {
-    type: String,
-    required: true,
-    unique: true,
-  },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    preference: {
+        type: String,
+        required: true,
+    },
 });
 
 const User = mongoose.model("User", userSchema);
 
-async function createUser(user) {
-    await user.save();
-}
+connect();
 
 app.get("/", (req, res) => {
-  res.render("index");
+    const successFlash = req.flash('success', 'Successfully Registered!');
+    res.render("index", {successFlash});
 });
 
 app.get("/register", (req, res) => {
-  res.render("register");
+    const errorFlash = req.flash('error');
+    res.render("register", {errorFlash});
 });
 
-app.post("/register", (req, res) => {
-  const email = req.body.email;
-  const password = req.body.password;
-  const preference = req.body.preference;
-  const newUser = new User({
-    email: req.body.email,
-    password: req.body.password,
-    preference: req.body.preference,
-  });
-  connect();
-  createUser(newUser);
-  res.redirect("/");
+app.post("/register", async (req, res) => {
+    const user = await User.findOne({ email: req.body.email });
+    if (user) {
+        req.flash('error', 'Our server puppies found someone with that email!');
+        return res.redirect("/register");
+    } else {
+        const newUser = new User({
+            email: req.body.email,
+            password: req.body.password,
+            preference: req.body.preference,
+        });
+        await newUser.save();
+        req.flash('success', 'User registration successful.');
+        return res.redirect("/");
+    }
 });
 
 app.listen(process.env.PORT || 8000, () => {
-  console.log(`Server listening on PORT ${process.env.PORT}.`);
+    console.log(`Server listening on PORT ${process.env.PORT}.`);
 });
